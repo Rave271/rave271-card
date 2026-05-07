c8dbfc79135db05fa22e72156ad90a18aa03de60
Rave271 <271raghavverma@gmail.com>
2026-05-07 14:21:50 +0530

    v4

---  api/card.js
+++  api/card.js
@@ -1,15 +1,4 @@
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "image/svg+xml");
-
-  res.status(200).send(`
-    <svg xmlns="http://www.w3.org/2000/svg" width="800" height="400">
-      <rect width="100%" height="100%" fill="#0f0f0f"/>
-      <text x="50%" y="50%" 
-            text-anchor="middle" 
-            fill="#f5f0e8" 
-            font-size="40">
-        RAVE271 WORKS
-      </text>
-    </svg>
-  `);
-}
+module.exports = (req, res) => {
+  res.setHeader("Content-Type", "text/plain");
+  res.status(200).send("API WORKS");
+};
