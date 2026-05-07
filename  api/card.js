af1e39a86872aee5eda2844132dac942f1584920
Rave271 <271raghavverma@gmail.com>
2026-05-07 14:20:08 +0530

    v3

---  api/card.js
+++  api/card.js
@@ -1,7 +1,15 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
   res.setHeader("Content-Type", "image/svg+xml");
-  res.setHeader("Cache-Control", "s-maxage=3600");
-  res.status(200).send(`<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100">
-    <rect width="100" height="100" fill="red"/>
-  </svg>`);
+
+  res.status(200).send(`
+    <svg xmlns="http://www.w3.org/2000/svg" width="800" height="400">
+      <rect width="100%" height="100%" fill="#0f0f0f"/>
+      <text x="50%" y="50%" 
+            text-anchor="middle" 
+            fill="#f5f0e8" 
+            font-size="40">
+        RAVE271 WORKS
+      </text>
+    </svg>
+  `);
 }
